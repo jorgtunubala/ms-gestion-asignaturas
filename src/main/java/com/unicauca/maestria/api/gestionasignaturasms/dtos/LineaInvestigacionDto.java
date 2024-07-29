0c6860603cfb3b43ff9a393e3f5c4fd784121f84
juliomellizo <117930717+juliomellizo@users.noreply.github.com>
2024-07-29 08:53:04 -0500

    Modificacion LineaInvestigacion

--- src/main/java/com/unicauca/maestria/api/gestionasignaturasms/dtos/LineaInvestigacionDto.java
+++ src/main/java/com/unicauca/maestria/api/gestionasignaturasms/dtos/LineaInvestigacionDto.java
@@ -2,6 +2,8 @@ package com.unicauca.maestria.api.gestionasignaturasms.dtos;
 
 import javax.validation.constraints.NotBlank;
 
+import com.unicauca.maestria.api.gestionasignaturasms.domain.msestudiantedocente.CategoriaLinea;
+
 import lombok.*;
 
 @Getter
@@ -17,6 +19,6 @@ public class LineaInvestigacionDto {
     private String titulo;
 
     @NotBlank
-    private String categoria;
+    private CategoriaLinea categoria;
 }
 
